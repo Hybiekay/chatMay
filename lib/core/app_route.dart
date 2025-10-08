6e9d206e9f74999b4e4b7db98d0c0b18766c08f6
unknown <hybiekay2@gmail.com>
2025-10-08 12:02:42 +0100

    Splash navigation

--- lib/core/app_route.dart
+++ lib/core/app_route.dart
@@ -1,8 +1,14 @@
+import 'package:chatbox/screens/login.dart';
+import 'package:chatbox/screens/onboarding.dart';
+import 'package:chatbox/screens/sign_up.dart';
 import 'package:chatbox/screens/splash.dart';
 import 'package:flutter/widgets.dart';
 
 class AppRoute {
   Map<String, WidgetBuilder> routes = {
-    "splash" : (BuildContext context) => Splash(),
+    "splash": (BuildContext context) => Splash(),
+    "onboarding": (BuildContext context) => Onboarding(),
+    "login": (BuildContext context) => Login(),
+    "sign-up": (BuildContext context) => SignUp(),
   };
 }
