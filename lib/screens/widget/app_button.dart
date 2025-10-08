1a04831f832687670a0abfdc6d8f2d19f8a5873e
unknown <hybiekay2@gmail.com>
2025-10-08 11:45:17 +0100

    onboarding

---
+++ lib/screens/widget/app_button.dart
@@ -0,0 +1,33 @@
+import 'package:chatbox/core/constants/app_colors.dart';
+import 'package:chatbox/core/extensions/theme_extensions.dart';
+import 'package:chatbox_app/core/constant/app_color.dart';
+import 'package:chatbox_app/core/extensions/theme_extensions.dart';
+import 'package:flutter/material.dart';
+
+class AppButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final String text;
+  final Color backgroundColor;
+  const AppButton({
+    super.key,
+    required this.onPressed,
+    required this.text,
+    this.backgroundColor = AppColor.primary,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        height: 48,
+
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Center(child: Text(text, style: context.textTheme.bodyMedium)),
+      ),
+    );
+  }
+}
