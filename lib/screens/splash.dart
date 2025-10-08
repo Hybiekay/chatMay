8a16a1eeec668e26a0d9828d7d681e5408e87312
unknown <hybiekay2@gmail.com>
2025-10-08 11:51:54 +0100

    Splash navigation

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,32 +1,40 @@
 import 'package:chatbox/core/constants/app_colors.dart';
 import 'package:chatbox/core/constants/app_images.dart';
+import 'package:chatbox/core/extensions/theme_extensions.dart';
+import 'package:chatbox/provider/splash_provider.dart';
 import 'package:chatbox/screens/widget/image_widget.dart';
-import 'package:chatbox/screens/widget/theme_mode_switch.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Splash extends StatelessWidget {
   const Splash({super.key});
 
   @override
   Widget build(BuildContext context) {
+    context.read<SplashProvider>().init(context);
     return Scaffold(
-      backgroundColor: AppColor.getSplashColor(context),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ImageWidget(
-              imagePath: AppImages.logo,
-              color: AppColor.logoColor(context),
-            ),
-            Text(
-              "ChatBox",
-              style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                color: AppColor.textColor(context),
+      body: Container(
+        color: AppColor.getSplashColor(context),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ImageWidget(
+                imagePath: AppImages.logo,
+                color: AppColor.logoColor(context),
               ),
-            ),
-            ThemeModeSwitch(),
-          ],
+              Text(
+                "Chatbox",
+                style: context.textTheme.bodyLarge?.copyWith(
+                  color: AppColor.textColor(context),
+                  fontSize: 40,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+              // ThemeModeSwicth(),
+            ],
+          ),
         ),
       ),
     );
